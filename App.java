574915a83fd36ecbbf6b09ea543f454e569c0fdf
chakradharlearning <chakradharlearning@gmail.com>
2026-06-18 20:56:51 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.exp6.maven.pro2_example_jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! NIGGA" );
+    }
+}
